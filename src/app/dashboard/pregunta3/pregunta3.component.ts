50d69fde097d7517562a6d4c4d6747ecdb140479
Stephano <nafetsgary@outlook.es>
2024-07-03 19:56:47 -0500

    Iniciando pregunta 3

--- src/app/dashboard/pregunta3/pregunta3.component.ts
+++ src/app/dashboard/pregunta3/pregunta3.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Foto } from '../interfaces/photos';
+import { RickandmortyService } from '../services/rickandmorty.service';
 
 @Component({
   selector: 'app-pregunta3',
@@ -8,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './pregunta3.component.css'
 })
 export class Pregunta3Component {
+
+  listaFotos: Foto[] = [];
+  listaFotosConFiltro: Foto[] = [];
+
+
+  constructor(private rickiService: RickandmortyService) {
+
+    this.rickiService.getPhotos().subscribe(data => {
+      this.listaFotos = data;
+
+      // filtrar las fotos que inicien con las letras "a", "r" o "s" en el titulo
+      this.listaFotosConFiltro = this.listaFotos.
+        filter(foto => foto.title!.charAt(0).toLowerCase() === 'a'
+          || foto.title!.charAt(0).toLowerCase() === 'r'
+          || foto.title!.charAt(0).toLowerCase() === 's');
+
+    })
+
+  }
 
 }
